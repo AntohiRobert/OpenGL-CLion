97f95bc22e7db089d00cac1e52de2dc5e34528aa
AntohiRobert <antohir@gmail.com>
2020-11-05 12:33:28 +0200

    --changed with examples and commented glfwWindowHint

--- main.cpp
+++ main.cpp
@@ -1,23 +1,28 @@
-#include <glad/glad.h>
+//#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+const int gridSize = 32;
+
+const int SCREEN_WIDTH = gridSize*32;
+const int SCREEN_HEIGHT = gridSize*32;
 
 int main()
 {
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
+    //glfwWindowHint doesn't work on some environments, Such as Win10. Uncomment.... idk when
+    /*
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    */
 
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
@@ -25,7 +30,7 @@ int main()
 
     // glfw window creation
     // --------------------
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "OpenGL-CLion", NULL, NULL);
     if (window == NULL)
     {
         std::cout << "Failed to create GLFW window" << std::endl;
@@ -34,7 +39,9 @@ int main()
     }
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
-
+    //Uncomment if you need glad. Might be a problem with glLoadIdentity() and glOrtho()
+    //Name collision in glad.h?
+    /*
     // glad: load all OpenGL function pointers
     // ---------------------------------------
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
@@ -42,7 +49,7 @@ int main()
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
-
+    */
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window))
@@ -53,8 +60,34 @@ int main()
 
         // render
         // ------
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        //glClear(GL_COLOR_BUFFER_BIT);
+        int width, height;
+        float ratio;
+        glfwGetFramebufferSize(window, &width, &height);
+        ratio = width / (float) height;
+        glViewport(0, 0, width, height);
         glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+
+
+        glOrtho(0, width, height, 0, 0, 1);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glBegin(GL_LINES);
+
+        for (int i = 0; i <= SCREEN_HEIGHT; i+=gridSize){
+            glVertex2f(0, i);
+            glVertex2f(SCREEN_WIDTH,i);
+        }
+
+        for (int i = 0; i <= SCREEN_WIDTH; i += gridSize){
+            glVertex2f(i,0);
+            glVertex2f(i, SCREEN_HEIGHT);
+        }
+        glEnd();
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
